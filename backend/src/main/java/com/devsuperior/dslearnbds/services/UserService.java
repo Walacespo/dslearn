a2fcba2c372fe5dd0894a88c38b434148ec7872e
Walace da Silva <walacespo@gmail.com>
2022-10-14 19:11:03 -0300

    Custom service level authorization

--- backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -22,6 +22,9 @@ public class UserService implements UserDetailsService {
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
+	private AuthService authService;
+	
+	@Autowired
 	private UserRepository repository;
 	
 	@Override
@@ -37,10 +40,9 @@ public class UserService implements UserDetailsService {
 	
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
+		authService.validateSelfOrAdmin(id);
 		Optional<User> obj = repository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new UserDTO(entity);
 	}
-	
-
 }
